988cf1900a12df1689e4456346169d852fa9757b
topherhooper <hooper.christopher@gmail.com>
2016-04-03 21:08:03 -0500

    refactor(): reformat to abstract

--- tf_tutorial/softmax_regression.py
+++ tf_tutorial/softmax_regression.py
@@ -2,6 +2,10 @@ import tensorflow as tf
 
 
 def load_mnist_data():
+    """
+    Loads tutorial data.
+    :return:
+    """
     from tensorflow.examples.tutorials.mnist import input_data
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
     return mnist
