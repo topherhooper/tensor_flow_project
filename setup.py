b5d826f99d0dc0a67d719a0e63d4d6631795ff82
topherhooper <hooper.christopher@gmail.com>
2016-04-03 20:31:03 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tensor_flow',
+    version='',
+    packages=['tf_tutorial'],
+    url='',
+    license='',
+    author='Christopher',
+    author_email='chooper@nextgxdx.com',
+    description='', requires=['tensorflow']
+)
